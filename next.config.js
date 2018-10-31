4b8b0ba76ecbca557757c88ad3b6a3bc40cab0fd
Keven Lefebvre <orditeck@gmail.com>
2018-10-31 12:38:56 -0400

    Add a server worker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withPlugins = require('next-compose-plugins');
 const sass = require('@zeit/next-sass');
+const offline = require('next-offline');
 
-module.exports = withPlugins([sass], {
+module.exports = withPlugins([sass, offline], {
     distDir: 'build'
 });
