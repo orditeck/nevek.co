84988f1888545dc26bd8aca551fc53f558c5a6dd
Keven Lefebvre <orditeck@gmail.com>
2018-10-31 20:58:52 -0400

    Add alts

--- components/LogoSlider.js
+++ components/LogoSlider.js
@@ -20,15 +20,15 @@ export default class extends Component {
         return <section className="logo-slider">
             <div className="swiper-container">
                 <div className="swiper-wrapper">
-                    <div className="swiper-slide"><img src="/static/images/logos/garderiedepot.jpg" /></div>
-                    <div className="swiper-slide"><img src="/static/images/logos/villesag.jpg" /></div>
-                    <div className="swiper-slide"><img src="/static/images/logos/uqac.jpg" /></div>
-                    <div className="swiper-slide"><img src="/static/images/logos/frt.jpg" /></div>
-                    <div className="swiper-slide"><img src="/static/images/logos/gardaworld.jpg" /></div>
-                    <div className="swiper-slide"><img src="/static/images/logos/lacbeauport.jpg" /></div>
-                    <div className="swiper-slide"><img src="/static/images/logos/sthubert.jpg" /></div>
-                    <div className="swiper-slide"><img src="/static/images/logos/laparisienne.jpg" /></div>
-                    <div className="swiper-slide"><img src="/static/images/logos/arcticpower.jpg" /></div>
+                    <div className="swiper-slide"><img src="/static/images/logos/garderiedepot.jpg" alt="Garderie Dépôt" /></div>
+                    <div className="swiper-slide"><img src="/static/images/logos/villesag.jpg" alt="Ville Saguenay" /></div>
+                    <div className="swiper-slide"><img src="/static/images/logos/uqac.jpg" alt="Université du Québec à Chicoutimi" /></div>
+                    <div className="swiper-slide"><img src="/static/images/logos/frt.jpg" alt="Fenêtres Réjean Tremblay" /></div>
+                    <div className="swiper-slide"><img src="/static/images/logos/gardaworld.jpg" alt="Garda World" /></div>
+                    <div className="swiper-slide"><img src="/static/images/logos/lacbeauport.jpg" alt="Municipalité du Lac Beauport" /></div>
+                    <div className="swiper-slide"><img src="/static/images/logos/sthubert.jpg" alt="St-Hubert" /></div>
+                    <div className="swiper-slide"><img src="/static/images/logos/laparisienne.jpg" alt="La Parisienne" /></div>
+                    <div className="swiper-slide"><img src="/static/images/logos/arcticpower.jpg" alt="Arctic Power" /></div>
                 </div>
             </div>
         </section>
