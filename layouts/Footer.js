9ccb03918fcbecdc545d7b208486a804d2199ae3
Keven Lefebvre <orditeck@gmail.com>
2018-11-02 22:26:23 -0400

    New footer + better responsive

--- layouts/Footer.js
+++ layouts/Footer.js
@@ -1,26 +1,45 @@
 export default () => <footer id="site-footer">
-   <div className="container">
-       <h1>Entrons en contact</h1>
+    <div className="container">
+        <small>
+            <img src="/static/images/footer.png"
+                 alt="Keven Lefebvre, Stratège numérique, Consultant Web & Développeur Web Senior à Chicoutimi, Saguenay."/>
+            Keven Lefebvre © {new Date().getFullYear()}, tous droits réservés. <br/>
+            Stratège numérique, Consultant Web & Développeur Web Senior à Chicoutimi, Saguenay.
+        </small>
+    </div>
 
-       <section>
-           <article>
-               <p>
-                   <strong>☎️ Téléphone</strong>
-                   <a href="tel://18444487602" className="btn">1 (844) 448-7602</a>
-               </p>
-           </article>
+    <div className="container">
+        <h1>Entrons en contact</h1>
 
-           <article>
-               <p>
-                   <strong>✉️ Courriel</strong>
-                   <a href="mailto:keven@nevek.co" className="btn">keven@nevek.co</a>
-               </p>
-           </article>
-       </section>
+        <section>
+            <article>
+                <p>
+                    <strong>Téléphone</strong>
+                    <a href="tel://18444487602" className="btn">1 (844) 448-7602</a>
+                </p>
+            </article>
 
-       <small>
-           Keven Lefebvre © {new Date().getFullYear()}, tous droits réservés. <br />
-           Stratège numérique, Consultant Web & Développeur Web Senior à Chicoutimi, Saguenay.
-       </small>
-   </div>
+            <article>
+                <p>
+                    <strong>Courriel</strong>
+                    <a href="mailto:keven@nevek.co" className="btn">keven@nevek.co</a>
+                </p>
+            </article>
+        </section>
+    </div>
+
+    <div className="container">
+        <small>
+            <img src="/static/images/footer.png"
+                 alt="Keven Lefebvre, Stratège numérique, Consultant Web & Développeur Web Senior à Chicoutimi, Saguenay."/>
+            Keven Lefebvre © {new Date().getFullYear()}, tous droits réservés. <br/>
+            Stratège numérique, Consultant Web & Développeur Web Senior à Chicoutimi, Saguenay. <br />
+            <a href="https://github.com/orditeck/nevek.co" target="_blank" rel="noopener noreferrer">Sources de ce site</a>
+            <a href="https://github.com/orditeck" target="_blank" rel="noopener noreferrer">Github</a>
+            <a href="https://www.instagram.com/keven.lefebvre" target="_blank" rel="noopener noreferrer">Instagram</a>
+            <a href="https://www.facebook.com/keven.lefebvre" target="_blank" rel="noopener noreferrer">Facebook</a>
+            <a href="https://www.linkedin.com/in/orditeck/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <br />&nbsp;
+        </small>
+    </div>
 </footer>
