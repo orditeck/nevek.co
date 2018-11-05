600cf7c7064d74af38a22ad1a758d1a2d1630763
Keven Lefebvre <orditeck@gmail.com>
2018-11-05 09:11:49 -0500

    Better slugify

--- components/FaqItem.js
+++ components/FaqItem.js
@@ -1,14 +1,9 @@
 import React, {Component} from 'react';
+import slugify from '../lib/slugify';
 
 export default class extends Component {
-    slugify = (text) => text.toString().toLowerCase().trim()
-        .replace(/[^\w\s-]/g, '') // remove non-word [a-z0-9_], non-whitespace, non-hyphen characters
-        .replace(/[\s_-]+/g, '_') // swap any length of whitespace, underscore, hyphen characters with a single _
-        .replace(/^-+|-+$/g, ''); // remove leading, trailing -
-
-
     render = () => {
-        const id = this.slugify(this.props.title || '');
+        const id = slugify(this.props.title || '');
 
         return <article className="faq">
             <input type="checkbox" id={id}/>
