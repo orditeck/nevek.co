5d7105c9ca8918919711de2db8c4db89f3084e84
Keven Lefebvre <orditeck@gmail.com>
2021-10-11 17:38:06 -0400

    Release at Mon Oct 11 17:38:06 EDT 2021

--- _next/static/smYGs9DToH7BT0AQdSEUC/pages/404.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"97Is":function(n,e,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){var n=t("uNx/");return{page:n.default||n}}])},"uNx/":function(n,e,t){"use strict";t.r(e);var u=t("q1tI"),o=t.n(u);e.default=function(){return o.a.createElement("section",{className:"404"},"Oopsi")}}},[["97Is",1,0]]]);
