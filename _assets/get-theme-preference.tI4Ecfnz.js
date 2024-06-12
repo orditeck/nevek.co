4957b45686243e14179479e386a7cc8013b8f1d0
Keven Lefebvre <kev@bqf.ca>
2024-06-12 19:48:35 -0400

    Release at Wed Jun 12 19:48:35 EDT 2024

--- _assets/get-theme-preference.tI4Ecfnz.js
+++
@@ -1,1 +0,0 @@
-const e=()=>typeof localStorage<"u"&&localStorage.getItem("theme")?localStorage.getItem("theme")||"light":window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light";export{e as g};
